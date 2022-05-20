1319b5bf46a370c3897a5f057b7f3970beca4032
liupingwei <liupingwei@shizhuang-inc.com>
2022-05-20 11:05:15 +0800

    添加跨域处理

--- app/application.js
+++ app/application.js
@@ -1,10 +1,19 @@
 var Koa = require('koa');
-// const Router = require('koa-router');
 const BodyParser = require('koa-bodyparser');
 const router = require('./routes/index');
+const cors = require('koa2-cors');
 const app = new Koa();
 
 const bparser = new BodyParser();
+
+// 处理跨域的配置
+app.use(cors({
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization', 'Date'],
+    maxAge: 100,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Custom-Header', 'anonymous'],
+}));
 
 app.use(bparser);
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,8 @@ const router = new Router({
     prefix: '/api'
 });
 
-router.get('/user', async (ctx, next) => {
-    console.log('request user api');
+router.get('/storys', async (ctx, next) => {
+    console.log('request storys api');
     // ctx.set({
     //     "Access-Control-Allow-Origin": "*", // 指定请求域，* 就是所有域名都可访问，即跨域打开
     //     // "Content-Type": "application/json",
@@ -16,7 +16,7 @@ router.get('/user', async (ctx, next) => {
     //     // "Content-Security-Policy": `script-src "self"` // 只允许页面`script`引入自身域名的地址
     // });
     ctx.response.type = 'html';
-    ctx.response.body = '<button>user is august rush982392173</button>';
+    ctx.response.body = '<text>storys</text>\n<button>user is august rush</button>';
     await next();
 });
 
